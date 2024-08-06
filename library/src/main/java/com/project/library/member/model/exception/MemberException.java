a95bd402ec9ef684c86692ab394104aa7ec2c7db
dogumi <hayoung1231dl@gmail.com>
2024-08-06 13:39:56 +0900

    Add files via upload

---
+++ library/src/main/java/com/project/library/member/model/exception/MemberException.java
@@ -0,0 +1,8 @@
+package com.project.library.member.model.exception;
+
+public class MemberException extends RuntimeException {
+	public MemberException() {}
+	public MemberException(String msg) {super(msg);}
+}
+
+
