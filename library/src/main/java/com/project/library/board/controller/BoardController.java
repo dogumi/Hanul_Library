34f2d7fe29f9e892cbf967d784ff1fd6d251770a
dogumi <hayoung1231dl@gmail.com>
2024-08-16 09:17:10 +0900

    시설대관, 자원봉사 추

--- library/src/main/java/com/project/library/board/controller/BoardController.java
+++ library/src/main/java/com/project/library/board/controller/BoardController.java
@@ -11,14 +11,39 @@ public class BoardController {
 
     @Autowired
     private BoardService bService;
-
+    
+    
+    
+    
+    // 인사말 이동
     @GetMapping("greetingView.bo")
     public String greetingView() {
         return "intro/greeting";
     }
     
+    // 부서 안내 이동
     @GetMapping("departmentView.bo")
     public String departmentView() {
     	return "intro/department";
+    }
+    
+    
+    // 시설대관 이동
+    @GetMapping("facillityRentalView.bo")
+    public String facillityRentalView() {
+    	return "application/facillityRental";
+    }
+    
+    // 자원봉사 이동
+    @GetMapping("volunteerView.bo")
+    public String volunteerView() {
+    	return "application/volunteer";
+    }
+    
+    
+    // 공지사항 이동
+    @GetMapping("noticeView.bo")
+    public String noticeView() {
+    	return "information/notice";
     }
 }
